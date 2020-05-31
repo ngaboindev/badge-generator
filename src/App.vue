77ea259384fdb3ab0d0decc083bc47d3c7da85c9
Caleb Denio <cjdenio44@gmail.com>
2020-05-31 09:26:33 -0400

    Got started

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
     <div class="struct-form">
-      <b-field label="Framework/Language Name">
-        <b-input></b-input>
+      <h1 class="title">Badge Generator</h1>
+      <b-field label="Type">
+        <b-select v-model="type">
+          <option value="framework">Framework</option>
+          <option value="language">Language</option>
+        </b-select>
       </b-field>
+      <b-field label="Framework/Language Name">
+        <b-input v-model="name"></b-input>
+      </b-field>
+      <b-field label="Icon Name" message="Get this from simpleicons.org">
+        <b-input v-model="icon"></b-input>
+      </b-field>
+      <b-field label="Badge Color" message="Get this from simpleicons.org, too">
+        <b-input v-model="color"></b-input>
+      </b-field>
+
+      <img :src="badgeUrl" style="display: block;margin-bottom:10px" />
+
       <b-button type="is-primary">Generate</b-button>
     </div>
   </div>
@@ -11,12 +27,34 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      name: "Vue",
+      icon: "vue.js",
+      type: "framework",
+      color: "4FC08D"
+    };
+  },
+  computed: {
+    badgeUrl() {
+      return `http://img.shields.io/static/v1?label=${
+        this.type == "framework" ? "Framework" : "Language"
+      }&message=${encodeURIComponent(
+        this.name || "React"
+      )}&color=${encodeURIComponent(
+        this.color || "blue"
+      )}&logo=${encodeURIComponent(
+        this.icon || "react"
+      )}&logoColor=white&style=for-the-badge`;
+    }
+  }
 };
 </script>
 
 <style>
-html, body {
+html,
+body {
   height: 100%;
 }
 #app {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import './style/style.scss'
 
 Vue.use(Buefy)
 
