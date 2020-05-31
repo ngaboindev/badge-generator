152d2570a9eaa59d6bc337dabb783e3f35873f94
cjdenio <cjdenio44@gmail.com>
2020-05-31 00:31:47 -0400

    Switched to Vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="struct-form">
+      <b-field label="Framework/Language Name">
+        <b-input></b-input>
+      </b-field>
+      <b-button type="is-primary">Generate</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+html, body {
+  height: 100%;
+}
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.struct-form {
+  padding: 30px;
+  width: 350px;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
+}
+</style>
